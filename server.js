d5bd609f459597eea904837ab5018c3a50b0938c
Ubuntu <ubuntu@ip-10-0-88-48.eu-west-1.compute.internal>
2015-01-31 13:45:19 +0000

    CONNECTION!!!1111!

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var net = require('net')
+var pennyingStrategy = require('./pennying.js')
 var c = require('./constants.js')
 var nextOrderId = 0
+var canTrade = false
+
+var pennying = {
+  BAR: [0,Infinity],
+  BAZ: [0,Infinity],
+  FOO: [0,Infinity],
+  QUUX: [0,Infinity],
+  CORGE: [0,Infinity]
+}
+
+var lines = []
 
 // Command line arguments override defaults
 if (process.argv.length == 4){
@@ -9,20 +21,43 @@ if (process.argv.length == 4){
 }
 
 var client = new net.Socket()
-client.connect(c.port,c.host,function(){
-	console.log('connected')
-
-client.write(JSON.stringify({
-		"type": "hello",
-		"team": c.team
-	}))
-})
-
 
 client.on('data',function(data){
-	console.log(data)
+	lines = data.toString().split("\n")
+	for (var i = 0; i < lines.length; i++){
+  	try{ lines[i] = JSON.parse(lines[i])}
+  	catch(e){}
+  	if (lines[i].type == 'book'){
+  	  for (var i = 0; i < book.buy.length; i++){
+    	  pennying[book.symbol][0] = Math.max(pennying[book.symbol][0],book.buy[i].)
+    	  
+  	  }    	
+  	}
+    else if (lines[i].type == 'hello'){
+      canTrade = lines[i].market_open
+      client.write(JSON.stringify(sell('BAR',1,10))+'\n')     
+    }
+    else if (lines[i].type == 'trade' && lines[i].symbol == 'BAR')
+      console.log("%d, %d",lines[i].price,lines[i].size)
+	}
 })
 
+client.on('closed',function(){
+  console.log('closed')
+})
+
+client.on('error',function(error){
+  console.log('error')
+})
+
+client.connect(c.port,c.host,function(){
+	console.log('connected')
+	
+  client.write(JSON.stringify({
+  		type: "hello",
+      team: c.team
+  	})+"\n")
+})
 
 // Return the correct JSON messages
 var buy = function(symbol, price, size){
--- test.py
+++ test.py
@@ -11,7 +11,7 @@ MESSAGE = '{"type":"hello","team":"LGW"}'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.sendall(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+data = s.recv(20)
 s.close()
 
 print "received data:", data
