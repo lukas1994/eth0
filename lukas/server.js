ae9366f09ff9e522a8fb6c75d0c0d1a4c4d3f773
Lukas Köbis <lukaskoebis@gmail.com>
2015-01-31 15:12:39 +0000

    test

--- lukas/server.js
+++ lukas/server.js
@@ -70,8 +70,12 @@ client.on('data',function(data){
     // strategy
     var corge = books['CORGE'], foo = books['FOO'], bar = books['BAR'];
     for (var sym in books) {
-      books[sym].buy = books[sym].buy.map(function(o) {return o[0];});
-      books[sym].sell = books[sym].sell.map(function(o) {return o[0];});
+      for (var i = 0; i < books[sym].buy; i++)
+        books[sym].buy[i] = books[sym].buy[i][0];
+      for (var i = 0; i < books[sym].sell; i++)
+        books[sym].sell[i] = books[sym].sell[i][0];
+      //books[sym].buy = books[sym].buy.map(function(o) {return o[0];});
+      //books[sym].sell = books[sym].sell.map(function(o) {return o[0];});
     }
     if (corge && foo && bar) {
           console.log('IN');
