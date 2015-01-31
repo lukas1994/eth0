04d1efcdefd4dcf1283ec465d31e555efc11ed99
Lukas Köbis <lukaskoebis@gmail.com>
2015-01-31 16:37:55 +0000

    test

--- lukas/server.js
+++ lukas/server.js
@@ -130,8 +130,6 @@ client.on('data',function(data){
 
     if (buy_corge+100 < (0.3*sell_foo + 0.8*sell_bar)) {
       convert('SELL', AMOUNT);
-      sell('FOO', sell_foo, Math.floor(0.3*AMOUNT));
-      sell('BAR', sell_bar, Math.floor(0.8*AMOUNT));
 
       /*buy('CORGE', buy_corge, AMOUNT);
       sell('FOO', sell_foo, Math.floor(0.3*AMOUNT));
@@ -139,7 +137,7 @@ client.on('data',function(data){
     }
     if ((0.3*buy_foo + 0.8*buy_bar)+100 < sell_corge) {
       convert('BUY', AMOUNT);
-      sell('CORGE', buy_corge, AMOUNT);
+
       /*sell('CORGE', buy_corge, AMOUNT);
       buy('FOO', sell_foo, Math.floor(0.3*AMOUNT));
       buy('BAR', sell_bar, Math.floor(0.8*AMOUNT));*/
